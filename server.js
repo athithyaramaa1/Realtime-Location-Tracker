3f63f4adf5908c9771f3001835c480689c92dc02
athithyaramaa1 <athithya.ramma@kalvium.community>
2024-09-24 17:00:16 +0530

    Add initial project files and configurations

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const socketio = require("socket.io");
+const path = require("path"); // Add this line to require the 'path' module
+
+const server = http.createServer(app);
+
+const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public"))); // Corrected _dirname to __dirname
+
+io.on("connection", (socket) => {
+  socket.on("sendLocation", (data) => {
+    io.emit("receive-location", { id: socket.id, ...data });
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("user-disconnected", socket.id);
+  });
+  console.log("New WS Connection");
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
